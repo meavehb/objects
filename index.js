0f9e0893eaf36da6f1a69f4cd44ef7ae3f745130
Hannah Meave Bautista <hannahmeave.bautista.iics@ust.edu.ph>
2020-02-20 16:45:26 +0800

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-//Using Objects :
-let stud = {name: "Juan Dela Cruz", section: "2ITE", age: 18};
+//Using factory function
+//createStudent is a factory function
+function createStudent(name, section, age){
+    let stud = {};
+    stud.name = name;
+    stud.section = section;
+    stud.age = age;
+    return stud;
+}
 
-console.log(stud.name);
+let s1 = createStudent("Pedro Morales", "2ITE", 20);
+document.write(`<h1>${s1.name}, ${s1.age}, is enrolled in ${s1.section}</h1>`)
