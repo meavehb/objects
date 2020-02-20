035b3c0aaa78a7075f2667ac82b8ba2eef66e509
Hannah Meave Bautista <hannahmeave.bautista.iics@ust.edu.ph>
2020-02-20 17:15:20 +0800

    tenth commit

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-//Using bind
-let s1 = {fname: "Juan", lname: "Dela Cruz"};
-let s2 = {fname: "Pedro", lname: "Reyes"};
-
-let joinName = function(){
-    console.log(`${this.fname} ${this.lname}`);
+//Using non-static
+class Dog{
+    constructor(name, breed){
+        this.name = name;
+        this.breed = breed;
+    }
+    eat(food){
+        console.log(`${this.name} eats plenty of ${food}`);
+    }
 }
 
-let y = joinName.bind(s1);
-y();
+let sissi = new Dog("Sissi", "Corgi");
+sissi.eat("meat");
